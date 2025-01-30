272542bff43ca55233f60d35f3f3b81c3571e8f7
JAYAKUMAR S <kumarjaya4196@gmail.com>
2025-01-30 22:48:44 +0530

    Add files via upload

---
+++ SumOfEvenNumbers.java
@@ -0,0 +1,13 @@
+public class SumOfEvenNumbers {
+    public static void main(String[] args) {
+        int sum = 0; // To store the sum of even numbers
+        int number = 2; // Start with the first even number
+
+        while (number <= 100) {
+            sum += number; // Add the current even number to the sum
+            number += 2; // Move to the next even number
+        }
+
+        System.out.println("The sum of all even numbers between 1 and 100 is: " + sum);
+    }
+}
